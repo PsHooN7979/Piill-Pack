7b62802e24a839055a1cedfc055725fc2300e12b
Starbox <39715563+Starbox7@users.noreply.github.com>
2024-04-30 18:14:12 +0900

    style update

--- app/src/domains/home/MainPage.js
+++ app/src/domains/home/MainPage.js
@@ -1,23 +1,21 @@
 import BottomNavigation from "../../common/components/BottomNavigation";
 
 function MainPage() {
-
-    return(
-        <div className="relative">
-
-            <BottomNavigation active="1" />
-            {/* 배경 */}
-            <div className="absolute bottom-0 w-full">
-                <div className="relative w-full h-auto bottom-0">
-                <img
-                    src="/web/images/wave.png"
-                    alt="Background"
-                    className="fixed bottom-0 w-full h-60 z-[-1]"
-                />
-                </div>
-            </div>
+  return (
+    <div className="relative">
+      <BottomNavigation active="4" />
+      {/* 배경 */}
+      <div className="absolute bottom-0 w-full">
+        <div className="relative w-full h-auto bottom-0">
+          <img
+            src="/web/images/wave.png"
+            alt="Background"
+            className="fixed bottom-0 w-full h-60 z-[-1]"
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default MainPage;
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,36 +3,35 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-      extend: {
-        fontSize: {
-          xxs: "0.5rem",
-          xxxs: "0.3rem"
-        },
-        boxShadow: {
-          'custom01': '1px 2px 4px rgba(0, 0, 0, 0.5)'
-        },
-        colors: {
-          "mint01": "#D6FCF5",
-          "mint02": "#7FDAD7",
-          "mint03": "#22B1AC",
-          "warn01": "#F2D0BE",
-          "warn02": "#E8A1A0"
-        },
-        textShadow: {
-          'default': '0 2px 4px rgba(0, 0, 0, 0.10)',
-          'md': '0 4px 6px rgba(0, 0, 0, 0.10)',
-          'lg': '0 6px 8px rgba(0, 0, 0, 0.10)',
-          'custom01': '1px 2px 4px rgba(0, 0, 0, 0.5)'
-        },
-        dropShadow: { // 드롭 쉐도우 추가
-          'custom': '1px 2px 4px rgba(0, 0, 0, 0.5)'
-        }
+    extend: {
+      fontSize: {
+        xxs: "0.5rem",
+        xxxs: "0.3rem",
       },
+      boxShadow: {
+        custom01: "1px 2px 4px rgba(0, 0, 0, 0.5)",
+      },
+      colors: {
+        mint01: "#D6FCF5",
+        mint02: "#7FDAD7",
+        mint03: "#22B1AC",
+        warn01: "#F2D0BE",
+        warn02: "#E8A1A0",
+      },
+      textShadow: {
+        default: "0 2px 4px rgba(0, 0, 0, 0.10)",
+        md: "0 4px 6px rgba(0, 0, 0, 0.10)",
+        lg: "0 6px 8px rgba(0, 0, 0, 0.10)",
+        custom01: "1px 2px 4px lightgray",
+      },
+      dropShadow: {
+        // 드롭 쉐도우 추가
+        custom: "1px 2px 4px ",
+      },
+    },
   },
   variants: {
-    dropShadow: ['responsive'] // 반응형 드롭 쉐도우 사용
+    dropShadow: ["responsive"], // 반응형 드롭 쉐도우 사용
   },
-  plugins: [
-    require('tailwindcss-textshadow')
-  ],
+  plugins: [require("tailwindcss-textshadow")],
 };
