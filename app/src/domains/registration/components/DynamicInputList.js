c3eb229d48ed2428055c692742f0e9ece78c8f83
gwonminsu <kd0615@naver.com>
2024-04-30 14:39:47 +0900

    중복 태그 버그 수정
    중복 태그 검사 시 공백 문자가 들어가면 중복으로 태그가 들어가는 버그 수정

--- app/src/domains/registration/components/DynamicInputList.js
+++ app/src/domains/registration/components/DynamicInputList.js
@@ -9,7 +9,7 @@ function DynamicInputList({ onItemsChange, placeholder }) {
             console.log("병명을 입력해주세요.");
             return;
         }
-        if (tags.includes(inputValue)) { // 이미 있을 경우
+        if (tags.includes(inputValue.replace(/\s+/g, ''))) { // 이미 있을 경우
             console.log("이미 추가된 병명입니다.");
             return;
         }
@@ -56,11 +56,9 @@ function DynamicInputList({ onItemsChange, placeholder }) {
                         {tag}
                         <button
                             onClick={() => removeTag(tag)}
-                            className="ml-2 rounded-full bg-red-200 hover:bg-red-300 text-red-600 p-1 inline-flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                            className="ml-2 hover:text-red-600 text-warn02 p-1 inline-flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
                         >
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
+                        x
                         </button>
                     </div>
                 ))}
