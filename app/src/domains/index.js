73ef8159522f2ac02073736ff6e7e0c423400c7c
Starbox <39715563+Starbox7@users.noreply.github.com>
2024-05-03 11:45:23 +0900

    build update

--- app/src/domains/index.js
+++ app/src/domains/index.js
@@ -1,7 +1,7 @@
 import Auth from "./auth/auth.page";
 import FistLogin from "./firstLogin/firstLogin";
 import Home from "./home/home";
-import Prescription from "./prescription/prescription.page";
+import Prescription from "./_prescription/prescription.page";
 
 const Pages = { Auth, FistLogin, Home, Prescription };
 
