bbb94cd605b02560a4b91877714da98fd85ab8ad
PsHooN7979 <144898565+PsHooN7979@users.noreply.github.com>
2024-05-07 18:19:08 +0900

    처방 목록
    50%

---
+++ app/src/domains/prescription/pill.detail.page.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import BottomNavigation from "../../common/components/BottomNavigation";
+import images from "../../constants/image.constant";
+import PillDetailHeader from "./components/presc.detail.header";
+
+export default function PillDetailPage() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="relative">
+      <PillDetailHeader title="약 상세 정보" />
+      <div className="flex justify-center items-center">
+        <div className="bg-opacity-100 w-[85%] min-h-screen">
+          
+        </div>
+      </div>
+
+      <BottomNavigation active="2" />
+      {/* 배경 */}
+      <div className="absolute bottom-0 w-full">
+        <div className="relative w-full h-auto bottom-0">
+          <img
+            src={images.wave}
+            alt="Background"
+            className="fixed bottom-0 w-full h-60 z-[-1]"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
