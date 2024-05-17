c1844fcb23b407640015cde27bcbfdb92fb628a3
gwonminsu <kd0615@naver.com>
2024-05-17 17:37:10 +0900

    유저 정보 리덕스 세팅
    + 오류 메시지 추가, 유저 정보 받는 메서드 추가(근데 토큰 관리해주는거 없어서 못함)

--- app/src/common/feature/store.js
+++ app/src/common/feature/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import nativeSlice from "../../domains/_scanner/features/slices/native.slice";
 import authSlice from "./slices/auth.slice";
 import snackBarSlice from "./slices/snackBar.slice";
+import userSlice from "./slices/user.slice";
 
 const store = configureStore({
   reducer: { 
     native: nativeSlice,
     auth: authSlice,
-    snackBar: snackBarSlice
+    snackBar: snackBarSlice,
+    user: userSlice,
   },
 });
 
