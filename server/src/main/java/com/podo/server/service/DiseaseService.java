ef645365c703405f78ad5ecf5595c2d9291149fa
PsHooN7979 <144898565+PsHooN7979@users.noreply.github.com>
2024-05-20 11:48:16 +0900

    질병 목록 불러오기
    .

--- server/src/main/java/com/podo/server/service/DiseaseService.java
+++ server/src/main/java/com/podo/server/service/DiseaseService.java
@@ -1,36 +1,47 @@
-//package com.podo.server.service;
-//
-//
-//import com.podo.server.dto.DiseaseDto;
-//import com.podo.server.entity.DiseaseEntity;
-//import com.podo.server.repository.DiseaseRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//import java.time.LocalDateTime;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class DiseaseService {
-//
-//    private final DiseaseRepository diseaseRepository;
-//
-//    // 질병 추가 기능
-//
-//    public DiseaseEntity saveDisease(DiseaseDto dto) {
-//        LocalDateTime now = LocalDateTime.now();
-//
-//        DiseaseEntity diseaseEntity = DiseaseEntity.builder()
-//                .name(dto.getName())
-//                .symptoms(dto.getSymptoms())
-//                .prevention(dto.getPrevention())
-//                .cause(dto.getCause())
-//                .created(now)
-//                .updated(now)
-//                .build();
-//
-//        return diseaseRepository.save(diseaseEntity);
-//    }
-//
-//
-//}
+package com.podo.server.service;
+
+
+import com.podo.server.dto.DiseaseDto;
+import com.podo.server.entity.DiseaseEntity;
+import com.podo.server.repository.DiseaseRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class DiseaseService {
+
+    private final DiseaseRepository diseaseRepository;
+
+    // 질병 추가 기능
+
+    // 질병 조회 기능
+    public List<DiseaseDto> getAllDiseases(){
+        List<DiseaseEntity> diseases = diseaseRepository.findAll();
+        return diseases.stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+    private DiseaseDto convertToDto(DiseaseEntity entity){
+        LocalDateTime now = LocalDateTime.now();
+        return DiseaseDto.builder()
+                .name(entity.getName())
+                .updated(now)
+                .symptoms(entity.getSymptoms())
+                .created(now)
+                .cause(entity.getCause())
+                .prevention(entity.getPrevention())
+                .build();
+    }
+
+
+
+
+}
