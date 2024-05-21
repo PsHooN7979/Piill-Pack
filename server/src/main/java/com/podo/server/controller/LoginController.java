bc4698703ef4cb2592b4d8438a386b46e93a0498
Starbox <39715563+Starbox7@users.noreply.github.com>
2024-05-21 11:45:54 +0900

    add

--- server/src/main/java/com/podo/server/controller/LoginController.java
+++ server/src/main/java/com/podo/server/controller/LoginController.java
@@ -31,10 +31,18 @@ public class LoginController {
         return "ok";
     }
 
+    // 최초 로그인
+    @PutMapping("/member/info")
+    public ResponseEntity<String> registerInfo(@RequestBody @Valid PatientDto dto,
+            @RequestHeader("Authorization") String token) {
+        try {
+            String email = jwtUtil.getUsername(token.substring(7)); // 토큰에 저장된 이메일 정보 가져옴
+            patientService.registerInfo(dto, email); // 서비스 로직 불러움
 
-
-    //최초 로그인
-
+            return new ResponseEntity<>("Information saved successfully!", HttpStatus.OK); // 성공
+        } catch (BusinessLogicException ex) {
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST); // 실패
+        }
+    }
 
 }
-
